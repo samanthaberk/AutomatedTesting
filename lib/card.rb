4a5d188d8873f621f657b7eae51d50c39d1c3526
Samantha Berk <sberk10@gmail.com>
2018-02-26 14:06:39 -0800

    changed ArgumentError code in initialization to unless statements to ensure passing tests and wrote code for #to_str

--- lib/card.rb
+++ lib/card.rb
@@ -7,12 +7,21 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
-
+    raise ArgumentError.new("The value of a card must be between 1 and 13.") unless @value > 0 && @value < 14
+    raise ArgumentError.new("The suit of a card must be hearts, spades, clubs, or diamonds.") unless @suit == :hearts || @suit == :spades || @suit == :diamonds || @suit == :clubs
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    case value
+    when 2, 3, 4, 5, 6, 7, 8, 9, 10
+      return "#{value} of #{suit.to_s}"
+    when 11
+      return "Jack of #{suit.to_s}"
+    when 12
+      return "Queen of #{suit.to_s}"
+    when 13
+      return "King of #{suit.to_s}"
+    end
   end
 
 end
